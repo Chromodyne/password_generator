90bbf6f5b47d7e8433a8eeb9f74164dff5e1af5d
chromodyne <dsinitiere@outlook.com>
2022-06-22 15:19:31 -0700

    Began roughly outlining generator functionality.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,7 +14,13 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 //Begin my code.
+// ====================================== //
+//Pseudo-code outline.
+//1. Prompt the user for their desired preferences and then store them.
 //Use an object to store the user's desired preferences for password generation?
+//Constructor to build the object's properties.
+//2. 
+
 
 //User Preferences Constructor
 function UserPreferences(length, lower, upper, numeric, special) {
@@ -26,10 +32,20 @@ function UserPreferences(length, lower, upper, numeric, special) {
 
 }
 
-
-generateBtn.addEventListener("click",);
+generateBtn.addEventListener("click",TakeUserInput);
 
 function TakeUserInput() {
 
-  UserPreferences(length)
+  //Create a new object.
+  let userPref = new UserPreferences();
+
+  //Utilize prompts to 
+  userPref.passLength = prompt("Please enter your desired password length (Between 8 & 128).");
+  userPref.includeUpper = prompt("Would you like to include upper-case letters in your password?");
+  userPref.includeNumeric = prompt("Would you like to include numbers in your password?");
+  userPref.includeSpecial = prompt("Would you like to include special characters in your password?");
+
+  console.log(userPref.passLength);
+
 }
+
