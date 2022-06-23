79de20eab77ab2371787b1cd0f526f749e827e58
chromodyne <dsinitiere@outlook.com>
2022-06-22 19:55:36 -0700

    Added character set array. Began generation logic.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,13 +2,13 @@
 var generateBtn = document.querySelector("#generate");
 
 // // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+  passwordText.value = password;
 
-// }
+}
 
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
@@ -23,19 +23,25 @@ var generateBtn = document.querySelector("#generate");
 
 
 //This will be a multi-dimensional array of possible characters.
-let upperChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
-
-let userPref = new UserPreferences();
+let charSet = [
+  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+  ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  ['!', '#', '%', '^', '&', '*', '(', ')']
+];
 
 //User Preferences Constructor
 function UserPreferences(length, lower, upper, numeric, special) {
 
   this.passLength = length;
+  this.includeLower = lower;
   this.includeUpper = upper;
   this.includeNumeric = numeric;
   this.includeSpecial = special; 
 
 }
+
+let userPref = new UserPreferences();
 
 generateBtn.addEventListener("click", takeUserInput);
 
@@ -43,15 +49,16 @@ function takeUserInput() {
 
   checkDesiredLength();
   checkDesiredCharTypes();
+  writePassword();
 
   //Debugging only.
   console.log(userPref.passLength);
+  console.log(userPref.includeLower);
   console.log(userPref.includeUpper);
   console.log(userPref.includeNumeric);
   console.log(userPref.includeSpecial);
 
 }
-
 
 //This function prompts the user to input a desired password length and then checks if it's within the accepted range of values.
 function checkDesiredLength() {
@@ -69,7 +76,7 @@ function checkDesiredLength() {
     return;
 
   } else {
-
+    
     alert(`Invalid range. Your password length must be between ${minChars} and ${maxChars}.`);
     return checkDesiredLength();
 
@@ -77,12 +84,49 @@ function checkDesiredLength() {
 
 }
 
+//Asks the user to confirm their desired character types to be used in the generated password. 
 function checkDesiredCharTypes() {
 
+  userPref.includeLower = window.confirm("Would you like to use lower case letters in your password?")
   userPref.includeUpper = window.confirm("Would you like to use upper case letters in your password?");
   userPref.includeNumeric = window.confirm("Would you like to use numeric characters in your password?");
   userPref.includeSpecial = window.confirm("Would you like to use special characters in your password?");
 
 }
 
+//Contains the logic to generate a password using the the users earlier defined preferences.
+function generatePassword() {
+
+  let password;
+
+  //Iterates through
+
+  switch (userPref) {
+
+    case (userPref.includeLower && userPref.includeUpper && userPref.includeNumeric && userPref.includeSpecial):
+
+      for (let i = 0; i < userPref.passLength; i++) {
+
+
+        // let randomizeNumber = Math.floor(Math.random * chars.length);
+        // password += chars.substring(randomizeNumber, randomizeNumber + 1);
+      }
+
+      alert(password);
+
+      break;
+
+    default:
+      break;
+
+  }
+
+  //Outline
+  //Check the value of each item in userPref.
+  //Generate a password based on that.
+  //Does not generate all possible passwords but only the one narrowed by the selection criteria.
+  //password = 
   
+  return password;
+
+}
